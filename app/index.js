a15f2b9fe2ed932e1068dfef609601f41a49bb73
Yoseph Anderson <yrganderson@gmail.com>
2016-11-17 23:26:21 -0700

    accept form input

--- app/index.js
+++ app/index.js
@@ -1,10 +1,56 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-class HelloMessage extends React.Component {
+class IdeaApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {items: [], text: ''};
+  }
+
   render() {
-    return <div>Hello {this.props.name}</div>;
+    return (
+      <div>
+        <h3>TODO</h3>
+        <IdeaList items={this.state.items} />
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={this.handleChange} value={this.state.text} />
+          <button>{'Add #' + (this.state.items.length + 1)}</button>
+        </form>
+      </div>
+    );
+  }
+
+  handleChange(e) {
+    this.setState({text: e.target.value});
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    var newItem = {
+      text: this.state.text,
+      id: Date.now()
+    };
+
+    this.setState((prevState) => ({
+      items: prevState.items.concat(newItem),
+      text: ''
+    }));
   }
 }
 
-ReactDOM.render(<HelloMessage name="world" />, document.getElementById("app"));
+class IdeaList extends React.Component {
+  render() {
+    return (
+      <ul>
+        {this.props.items.map(item => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+
+ReactDOM.render(<IdeaApp/>, document.getElementById("app"));
